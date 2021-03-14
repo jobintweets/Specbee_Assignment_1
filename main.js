545818c5891120b832fac5ef9748979cd75f9b5d
Jobin George <jobintweets@gmail.com>
2021-03-14 17:12:25 +0530

    added data fetching

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-function display_Content(event,idName) {
+function display_Content(event, idName) {
 
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("row3_col2");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    // tablinks = document.getElementsByClassName("tablinks");
-    // for (i = 0; i < tablinks.length; i++) {
-    //   tablinks[i].className = tablinks[i].className.replace(" active", "");
-    // }
-    document.getElementById(idName).style.display = "block";
-    event.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("row3_col2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  // tablinks = document.getElementsByClassName("tablinks");
+  // for (i = 0; i < tablinks.length; i++) {
+  //   tablinks[i].className = tablinks[i].className.replace(" active", "");
+  // }
+  document.getElementById(idName).style.display = "block";
+  event.currentTarget.className += " active";
 
 }
-function display_dropdown(event,idName){
-  
+
+function display_dropdown(event, idName) {
+
   document.getElementById(idName).style.display = "block";
- 
+
   event.currentTarget.className += " active";
 }
-function test(event,idName){
+
+function test(event, idName) {
 
   document.getElementById(idName).style.display = "none";
 }
+
 function test1(idName) {
- 
-  var content=document.getElementById(idName);
- if(content.value){
-  document.getElementById(idName).style.border="none";
- }
- if(!content.value){
-  document.getElementById(idName).style.border=" 2px solid #C8122E";
- }
+
+  var content = document.getElementById(idName);
+  if (content.value) {
+    document.getElementById(idName).style.border = "none";
+  }
+  if (!content.value) {
+    document.getElementById(idName).style.border = " 2px solid #C8122E";
+  }
 }
+
 function some(ide) {
 
-  document.getElementById(ide).style.display="block";
+  document.getElementById(ide).style.display = "block";
 }
